92a45920ec899ae3c227d2dc42436b1d1f5b3b90
DENNIS DOMINGO <dennisdomingo@MacBook-Pro.local>
2021-10-03 23:48:46 -0700

    Changed home page to include product card

--- app/main/payments.py
+++ app/main/payments.py
@@ -54,7 +54,7 @@ def adyen_payments(frontend_request):
 
 	payments_request = {
 		'amount': {
-			'value': 1000,
+			'value': 5000,
 			'currency': choose_currency(txvariant)
 		},
 		'channel': 'Web',
